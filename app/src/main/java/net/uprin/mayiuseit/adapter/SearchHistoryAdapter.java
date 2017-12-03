93e65ca89babf9d92c0d7f6421f748f42edd72c3
JaeSeung <wotmd321@gmail.com>
2017-12-03 21:12:32 +0900

    검색 히스토리 구현

---
+++ app/src/main/java/net/uprin/mayiuseit/adapter/SearchHistoryAdapter.java
@@ -0,0 +1,63 @@
+package net.uprin.mayiuseit.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import net.uprin.mayiuseit.R;
+import net.uprin.mayiuseit.model.History;
+
+import java.util.List;
+
+/**
+ * Created by CJS on 2017-12-03.
+ */
+
+public class SearchHistoryAdapter extends RecyclerView.Adapter<SearchHistoryAdapter.SearchViewHolder> {
+
+    List<History> histories;
+    private int rowLayout;
+    private Context context;
+
+    public static class SearchViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout historyLayout;
+        TextView history_keyword;
+
+
+        public SearchViewHolder(View v) {
+            super(v);
+            historyLayout = (LinearLayout) v.findViewById(R.id.history_recycler_view);
+            history_keyword = (TextView) v.findViewById(R.id.history_keyword);
+        }
+    }
+
+    public SearchHistoryAdapter(List<History> histories, int rowLayout, Context context) {
+        this.histories = histories;
+        this.rowLayout = rowLayout;
+        this.context = context;
+    }
+
+    @Override
+    public SearchHistoryAdapter.SearchViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                    int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout, parent, false);
+        return new SearchViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(SearchViewHolder holder, final int position) {
+        holder.history_keyword.setText(histories.get(position).getKeyword());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return histories.size();
+    }
+
+}
---
+++ app/src/main/java/net/uprin/mayiuseit/model/History.java
@@ -0,0 +1,22 @@
+package net.uprin.mayiuseit.model;
+
+/**
+ * Created by CJS on 2017-12-03.
+ */
+
+public class History {
+
+    private String keyword;
+
+    public History(String keyword){
+        this.keyword = keyword;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}

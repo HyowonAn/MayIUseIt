2aec14c1f1316c83119395c86d379f49d49e451a
Hyowon An <gydnjsdl1234@ajou.ac.kr>
2017-12-10 21:18:07 +0900

    fcm

---
+++ app/src/main/java/net/uprin/mayiuseit/firebase/FirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package net.uprin.mayiuseit.firebase;
+
+/**
+ * Created by uPrin on 2017. 12. 10..
+ */
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+    }
+
+}
